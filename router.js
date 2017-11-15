7788400386d6ed372c59ba2bc8dd269a74aaa202
Maks <pvl1720@gmail.com>
2017-11-14 23:09:24 -0800

    expanding

--- router.js
+++ router.js
@@ -2,3 +2,14 @@ function route(pathname){
   console.log('ABout to route a request for ' + pathname);
 }
 exports.route = route;
+
+function start() {
+  console.log('Request handler \'start\' was called');
+}
+
+function upload() {
+  console.log('Request handler \'upload\' was called');
+}
+
+exports.start = start;
+exports.upload = upload;
